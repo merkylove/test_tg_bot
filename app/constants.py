3c485577c09f0802fbfbf6c0961f9acf5753b70d
merkylove <merkylove@users.noreply.github.com>
2018-12-10 05:19:25 +0300

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -7,7 +7,7 @@ NAME, AGE, GENDER, CITY, COUNTRY = 'name', 'age', 'gender', 'city', 'country'
 FIELDS = (NAME, AGE, GENDER, CITY, COUNTRY)
 
 # age is chosen from predefined options
-AGE_OPTIONS = [['male', 'female', 'other']]
+AGE_OPTIONS = (('male', 'female', 'other'),)
 GENDER_REPLY_MARKUP = ReplyKeyboardMarkup(AGE_OPTIONS, one_time_keyboard=True)
 CHOICE_FIELDS = {
     GENDER: GENDER_REPLY_MARKUP
